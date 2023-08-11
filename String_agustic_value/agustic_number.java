66f7615001faeec0c358402e320aae7f85683de3
Sudhakar.D <96967364+sudhakar17White@users.noreply.github.com>
2023-08-11 09:16:18 -0700

    Update agustic_number.java

--- String_agustic_value/agustic_number.java
+++ String_agustic_value/agustic_number.java
@@ -4,12 +4,12 @@ public class agustic_number {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         String num=s.nextLine();
-        int sum=Integer.parseInt(num.substring(0,1))+Integer.parseInt(num.substring(1,num.length()));
-        sum=(Integer.parseInt(num))+sum+(Integer.parseInt(num.substring(0,2))+Integer.parseInt(num.substring(2,num.length())));
-        for(int i=0;i<num.length();i++)
+        int sum=0,add=0;
+        for(int i=1;i<=num.length()-1;i++)
         {
-            sum=sum+Integer.parseInt(""+num.charAt(i));
+            sum=sum+(Integer.parseInt(num.substring(0,i))+Integer.parseInt(num.substring(i, num.length())));  
+            add=add+Integer.parseInt(""+num.charAt(i));
         }
-        System.out.println(sum);
+        System.out.println(sum+Integer.parseInt(num)+add+Integer.parseInt(""+num.charAt(0)));
     }
 }
