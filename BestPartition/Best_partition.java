67bae3dbe5d63cdcbf6a8abfabf5e68d3270ad74
Sudhakar.D <96967364+sudhakar17White@users.noreply.github.com>
2023-08-13 09:41:54 -0700

    Add files via upload

---
+++ BestPartition/Best_partition.java
@@ -0,0 +1,53 @@
+package BestPartition;
+import java.util.*;
+public class Best_partition {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int ar[]={5,6,6,8};
+        int len=4;
+        if(ar.length==len)
+        {
+            int min=ar[0];
+            for(int i=1;i<ar.length;i++)
+            {
+                if(ar[i]<min)
+                {
+                    min=ar[i];
+                }
+            }
+            System.out.println(min);
+        }
+        else
+        {
+            ArrayList<Integer> arl=new ArrayList<>();
+            int si=(ar.length/len)+1;
+            System.out.println(si);
+            while(si!=0)
+            {
+                int odd=0,even=0,in=0;
+                for(int i=0;i<ar.length;i++)
+                {
+
+                    if(in==si)
+                    {
+                        even=even+ar[i];
+                    }
+                    else
+                    {
+                        odd=odd+ar[i];
+                    }
+                        in++;
+                }
+                if(even<odd)
+                {
+                   // System.out.println(even);
+                    arl.add(even);
+                }
+                
+                si--;
+            }
+           // System.out.println(arl);
+            System.out.println(Collections.max(arl));
+        }
+    }
+}
