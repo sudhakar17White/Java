7d564f49d902309cddfa69c8337604969966c010
Sudhakar.D <96967364+sudhakar17White@users.noreply.github.com>
2023-11-02 14:38:01 +0530

    Add files via upload

--- LEETCODE/ValidPalindrome.java
+++ LEETCODE/ValidPalindrome.java
@@ -2,8 +2,15 @@ import java.util.Arrays;
 
 public class ValidPalindrome {
    public static void main(String[] args) {
-        String a="A man, a plan, a canal: Panama";
+        String a="a.";
+        if(a.length()==1)
+        {
+            System.out.println("palinrome");
+        }
+        else
+        {
         String ar[]=a.split(" ");
+        
         String af="";
         for(int i=0;i<ar.length;i++)
         {
@@ -13,18 +20,23 @@ public class ValidPalindrome {
         String ans="";
         for(int i=0;i<af.length();i++)
         {
-            if(Character.isAlphabetic(af.charAt(i)))
+            if(Character.isLetterOrDigit(af.charAt(i)))
             {
                 ans=ans+(af.charAt(i));
             }
         }
-        
+        System.out.println(ans);
         StringBuilder str=new StringBuilder(ans);
         String res=""+str.reverse();
-        if(res.equals(ans))
+        if(res.equals(ans)&&res.length()==1)
         {
             System.out.println("palinrome");
         }
+        else
+        {
+            System.out.println("not");
+        }
+    }
         
    } 
 }
